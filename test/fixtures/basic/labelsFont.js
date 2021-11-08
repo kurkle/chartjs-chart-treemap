b1fe70cae6bc2c8ca14609dcb0476b401e61b4ec
stockiNail <stocki.nail@gmail.com>
2021-11-08 12:04:23 +0100

    adds test cases

---
+++ test/fixtures/basic/labelsFont.js
@@ -0,0 +1,33 @@
+export default {
+  tolerance: 0.0040,
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        label: 'Simple treemap',
+        data: [6, 6, 4, 3, 2, 2, 1],
+        backgroundColor: 'red',
+        labels: {
+          display: true,
+          align: 'center',
+          position: 'top',
+          font: {
+            size: 16,
+            weight: 'bold'
+          }
+        }
+      }]
+    },
+    options: {
+      layout: {
+        padding: 20
+      }
+    }
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};
---
+++ test/fixtures/grouped/captionsAlign.js
@@ -0,0 +1,48 @@
+const data = [
+  {category: 'main', subcategory: 'one', value: 1},
+  {category: 'main', subcategory: 'one', value: 2},
+  {category: 'main', subcategory: 'one', value: 3},
+  {category: 'main', subcategory: 'two', value: 5},
+  {category: 'main', subcategory: 'two', value: 1},
+  {category: 'main', subcategory: 'two', value: 1},
+  {category: 'other', subcategory: 'one', value: 4},
+  {category: 'other', subcategory: 'one', value: 5},
+  {category: 'other', subcategory: 'two', value: 2},
+  {category: 'other', subcategory: 'two', value: 6},
+];
+
+export default {
+  tolerance: 0.0040,
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        tree: data,
+        key: 'value',
+        groups: ['category', 'subcategory', 'value'],
+        backgroundColor: 'lightGreen',
+        captions: {
+          display: true,
+          align: 'center'
+        },
+        labels: {
+          display: true,
+          formatter(ctx) {
+            return ctx.type === 'data' ? ctx.raw.v : '';
+          }
+        }
+      }]
+    },
+    options: {
+      layout: {
+        padding: 20
+      }
+    }
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};

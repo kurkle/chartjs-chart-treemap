ce43ab1fed9bebc77ca7d83be7ae686ae02eae45
stockiNail <stocki.nail@gmail.com>
2022-09-22 10:27:27 +0200

    Enable borderRadius to the rectangles of treemap elements

---
+++ test/fixtures/basic/borderRadius.js
@@ -0,0 +1,24 @@
+export default {
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        label: 'Simple treemap',
+        data: [6, 6, 4, 3, 2, 2, 1],
+        backgroundColor: 'red',
+        borderRadius: 6
+      }]
+    },
+    options: {
+      layout: {
+        padding: 20
+      }
+    }
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};
---
+++ test/fixtures/basic/borderRadiusIsIgnored.js
@@ -0,0 +1,29 @@
+export default {
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        label: 'Simple treemap',
+        data: [6, 6, 4, 3, 2, 2, 1],
+        backgroundColor: 'red',
+        borderColor: 'black',
+        borderRadius: 6,
+        borderWidth: {
+          top: 5,
+          bottom: 5
+        }
+      }]
+    },
+    options: {
+      layout: {
+        padding: 20
+      }
+    }
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};

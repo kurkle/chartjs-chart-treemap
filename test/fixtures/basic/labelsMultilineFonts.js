2d89431f87fa9f65033107f4514153218604dd35
stockiNail <stocki.nail@gmail.com>
2022-09-28 19:27:44 +0200

    Enable different fonts and colors configuration on multiline labels (#128)
    * Enable different fonts and colors configuration on multiline labels
    
    * fixes mpa key using new fonts key
    
    * adds test cases
    
    * adds type definitions
    
    * adds documentation
    
    * adds sample

---
+++ test/fixtures/basic/labelsMultilineFonts.js
@@ -0,0 +1,31 @@
+export default {
+  tolerance: 0.0150,
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        label: 'Simple treemap',
+        data: [16, 16, 14, 13, 12, 12],
+        backgroundColor: 'red',
+        labels: {
+          display: true,
+          align: 'left',
+          position: 'top',
+          font: () => [{size: 24}, {size: 12}],
+          formatter(ctx) {
+            return ctx.type === 'data' ? ['The value is', ctx.raw.v + '', 'The value is', ctx.raw.v + ''] : [];
+          }
+        }
+      }]
+    },
+    options: {
+      events: []
+    }
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};

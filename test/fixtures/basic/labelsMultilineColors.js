8139ccd57989e4657b7bdafb2c936a51c97338f5
stockiNail <stocki.nail@gmail.com>
2022-09-28 12:26:26 +0200

    adds test cases

---
+++ test/fixtures/basic/labelsMultilineColors.js
@@ -0,0 +1,30 @@
+export default {
+  tolerance: 0.0115,
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        label: 'Simple treemap',
+        data: [16, 16, 14, 13, 12, 12],
+        labels: {
+          display: true,
+          align: 'left',
+          position: 'top',
+          color: () => ['red', 'green'],
+          formatter(ctx) {
+            return ctx.type === 'data' ? ['The value is', ctx.raw.v + '', 'The value is', ctx.raw.v + ''] : [];
+          }
+        }
+      }]
+    },
+    options: {
+      events: []
+    }
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};

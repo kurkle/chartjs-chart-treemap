8973aec0ef6ce9f1279602a301d309e58f08f83a
stockiNail <stocki.nail@gmail.com>
2022-10-10 15:16:31 +0200

    adds test case for ungrouped tree

---
+++ test/fixtures/grouped/treeUngrouped.js
@@ -0,0 +1,53 @@
+const data = {
+  A: {
+    C: {
+      C1: {
+        C1a: {
+          value: 6.25
+        },
+        C1b: {
+          value: 6.25
+        },
+      },
+      C2: {
+        value: 12.5
+      }
+    },
+    D: {
+      value: 25
+    }
+  },
+  B: {
+    value: 50,
+  },
+  G: {
+    C: {
+      value: 50
+    }
+  },
+};
+
+export default {
+  config: {
+    type: 'treemap',
+    data: {
+      datasets: [{
+        tree: data,
+        key: 'value',
+        captions: {
+          display: false
+        },
+        labels: {
+          display: true,
+        }
+      }]
+    },
+  },
+  options: {
+    spriteText: true,
+    canvas: {
+      height: 256,
+      width: 512
+    }
+  }
+};

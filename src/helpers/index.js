e67d7840a2856f714699ed9e2e36b63b4f30a36c
Jukka Kurkela <jukka.kurkela@gmail.com>
2022-10-13 00:23:12 +0300

    fix tests (dpr=1)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,14 +1,16 @@
 
 export function rasterizeRect(rect, dpr) {
-  const floor = v => Math.floor(v * dpr) / dpr;
-  const ceil = v => Math.ceil(v * dpr) / dpr;
-  const x = floor(rect.x);
-  const y = floor(rect.y);
+  if (dpr === 1) {
+    return rect;
+  }
+  const rasterize = v => Math.round(v * dpr) / dpr;
+  const x = rasterize(rect.x);
+  const y = rasterize(rect.y);
   return {
     ...rect,
     x,
     y,
-    w: ceil(x + rect.w) - x,
-    h: ceil(y + rect.h) - y,
+    w: rasterize(x + rect.w) - x,
+    h: rasterize(y + rect.h) - y,
   };
 }

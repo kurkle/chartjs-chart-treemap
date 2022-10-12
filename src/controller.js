f5bf527c1cbcb46cd9bed37eb50c5879ec8faf32
stockiNail <stocki.nail@gmail.com>
2022-10-12 18:13:24 +0200

    improves performance

--- src/controller.js
+++ src/controller.js
@@ -114,6 +114,10 @@ export default class TreemapController extends DatasetController {
    * will be implemented
    */
   updateRangeFromParsed(range, scale) {
+    if (range.update) {
+      return;
+    }
+    range.update = true;
     if (scale.id === 'x') {
       range.min = 0;
       range.max = 1;

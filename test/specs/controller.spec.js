e7e65f0360986f1db7ce8c255f90336da49ff40f
Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
2023-03-22 15:10:12 +0400

    Adds more cases for "grouping 3 levels of data" test

--- test/specs/controller.spec.js
+++ test/specs/controller.spec.js
@@ -30,7 +30,12 @@ describe('controller', function() {
       {key: 10, a: 'a1', b: 'b1', c: 'c1'},
       {key: 20, a: 'a1', b: 'b1', c: 'c1'},
       {key: 40, a: 'a2', b: 'b1', c: 'c1'},
-      {key: 99, a: 'a2', b: 'b1', c: 'c1'}
+      {key: 99, a: 'a2', b: 'b1', c: 'c1'},
+      {key: 10, a: 'a3', b: 'b1', c: 'c1'},
+      {key: 20, a: 'a3', b: 'b1', c: 'c2'},
+      {key: 40, a: 'a3', b: 'b2', c: 'c3'},
+      {key: 99, a: 'a3', b: 'b2', c: 'c4'},
+      {key: 50, a: 'a3', b: 'b3', c: 'c4'}
     ];
     const chart = acquireChart({
       type: 'treemap',
@@ -55,5 +60,29 @@ describe('controller', function() {
     const a2b1c1 = buildData.find((o) => o._data.path === 'a2.b1.c1');
     expect(a2b1c1.v).toBe(139);
     expect(a2b1c1._data.children.length).toBe(2);
+
+    const a3 = buildData.find((o) => o._data.path === 'a3');
+    expect(a3.v).toBe(10 + 20 + 40 + 99 + 50);
+    expect(a3._data.children.length).toBe(5);
+
+    const a3b1c1 = buildData.find((o) => o._data.path === 'a3.b1.c1');
+    expect(a3b1c1.v).toBe(10);
+    expect(a3b1c1._data.children.length).toBe(1);
+
+    const a3b1 = buildData.find((o) => o._data.path === 'a3.b1');
+    expect(a3b1.v).toBe(10 + 20);
+    expect(a3b1._data.children.length).toBe(2);
+
+    const a3b2 = buildData.find((o) => o._data.path === 'a3.b2');
+    expect(a3b2.v).toBe(40 + 99);
+    expect(a3b2._data.children.length).toBe(2);
+
+    const a3b3 = buildData.find((o) => o._data.path === 'a3.b3');
+    expect(a3b3.v).toBe(50);
+    expect(a3b3._data.children.length).toBe(1);
+
+    const a3b3c4 = buildData.find((o) => o._data.path === 'a3.b3.c4');
+    expect(a3b3c4.v).toBe(50);
+    expect(a3b3c4._data.children.length).toBe(1);
   });
 });

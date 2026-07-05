206f3ecd233c6bcab5516c11a1a72eb8c39d99e3
Jukka Kurkela <jukka.kurkela@gmail.com>
2026-07-05 12:27:42 +0300

    docs: migrate documentation to Astro

--- docs/scripts/register.js
+++ docs/scripts/register.js
@@ -1,22 +1,27 @@
-import {Chart, registerables} from 'chart.js';
-import {TreemapController, TreemapElement} from '../../dist/chartjs-chart-treemap.esm';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import zoomPlugin from 'chartjs-plugin-zoom';
+import { Chart, registerables } from 'chart.js'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+import zoomPlugin from 'chartjs-plugin-zoom'
 
-Chart.register(...registerables);
-Chart.register(TreemapController, TreemapElement, ChartDataLabels, zoomPlugin);
-Chart.defaults.plugins.datalabels.display = false;
+import { TreemapController, TreemapElement } from '../../dist/chartjs-chart-treemap.esm'
+
+Chart.register(...registerables)
+Chart.register(TreemapController, TreemapElement, ChartDataLabels, zoomPlugin)
+Chart.defaults.plugins.datalabels.display = false
 
 Chart.register({
-  id: 'version',
   afterDraw(chart) {
-    const ctx = chart.ctx;
-    ctx.save();
-    ctx.font = '9px monospace';
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-    ctx.textAlign = 'right';
-    ctx.textBaseline = 'top';
-    ctx.fillText('Chart.js v' + Chart.version + ' + chartjs-chart-treemap v' + TreemapController.version, chart.chartArea.right, 0);
-    ctx.restore();
-  }
-});
+    const ctx = chart.ctx
+    ctx.save()
+    ctx.font = '9px monospace'
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
+    ctx.textAlign = 'right'
+    ctx.textBaseline = 'top'
+    ctx.fillText(
+      `Chart.js v${Chart.version} + chartjs-chart-treemap v${TreemapController.version}`,
+      chart.chartArea.right,
+      0
+    )
+    ctx.restore()
+  },
+  id: 'version',
+})

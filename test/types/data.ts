2a1d1df08aea3f47a90b3aaed601ee2007336f54
Jukka Kurkela <jukka.kurkela@gmail.com>
2026-07-13 11:41:39 +0300

    docs: clarify TypeScript dataset requirement
    Fixes #196

---
+++ test/types/data.ts
@@ -0,0 +1,12 @@
+import type { ChartData } from 'chart.js'
+
+import '../../dist/index.esm'
+
+const _data: ChartData<'treemap'> = {
+  datasets: [
+    {
+      data: [],
+      tree: [15, 6, 6, 5, 4, 3, 2, 2],
+    },
+  ],
+}
